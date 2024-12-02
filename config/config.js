af497d986ee777b3e00c079a45a14d8d9512bde5
Fadga Dahaba Bashir Dev <138160871+fadgadahababashirdev@users.noreply.github.com>
2024-12-02 09:15:27 -0500

    creating Authors model

--- config/config.js
+++ config/config.js
@@ -1,11 +1,11 @@
 require('dotenv').config();
 module.exports = {
   development: {
-    username: 'root',
-    password: null,
-    database: 'database_development',
-    host: '127.0.0.1',
-    port: process.env.PORT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'postgres',
   },
   test: {
---
+++ db/models/author.js
@@ -0,0 +1,30 @@
+const { Sequelize } = require('sequelize');
+const sequelize = require('../../config/database');
+
+const Authors = sequelize.define('authors', {
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: Sequelize.INTEGER,
+  },
+  username: {
+    type: Sequelize.STRING,
+  },
+  email: {
+    type: Sequelize.STRING,
+  },
+  password: {
+    type: Sequelize.STRING,
+  },
+  createdAt: {
+    allowNull: false,
+    type: Sequelize.DATE,
+  },
+  updatedAt: {
+    allowNull: false,
+    type: Sequelize.DATE,
+  },
+});
+
+module.exports = Authors;
